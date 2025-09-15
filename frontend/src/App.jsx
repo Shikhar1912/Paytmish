89f3f5d02e0d44ae1c634d0f669314776d6f7a1d
shikhar <shikhartripathi1912@gmail.com>
2025-09-15 06:46:38 +0000

    added rerouting of main link to signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import SendMoney from "./pages/SendMoney";
+import { Navigate } from "react-router-dom";
 import { useStore } from "../store";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signup" replace />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
